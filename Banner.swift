4577ba761784c40ab4c8c1497f4d9cb70f87c9ec
Michael barney Junior <michaelbarneyjunior@gmail.com>
2019-10-22 09:48:17 -0300

    Added banner implementation

---
+++ Banner.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+import GoogleMobileAds
+import UIKit
+
+final private class BannerVC: UIViewControllerRepresentable  {
+
+    func makeUIViewController(context: Context) -> UIViewController {
+        let view = GADBannerView(adSize: kGADAdSizeBanner)
+
+        let viewController = UIViewController()
+        view.adUnitID = bannerID
+        view.rootViewController = viewController
+        viewController.view.addSubview(view)
+        viewController.view.frame = CGRect(origin: .zero, size: kGADAdSizeBanner.size)
+        view.load(GADRequest())
+
+        return viewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
+
+struct Banner:View{
+    var body: some View{
+        HStack{
+            Spacer()
+            BannerVC().frame(width: 320, height: 50, alignment: .center)
+            Spacer()
+        }
+    }
+}
+
+struct Banner_Previews: PreviewProvider {
+    static var previews: some View {
+        Banner()
+    }
+}
